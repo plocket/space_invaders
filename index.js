b5919aab485482d6d0b7fd37e56eed2fb5fff722
plocket <52798256+plocket@users.noreply.github.com>
2023-01-23 17:51:12 -0500

    Change vectors to args, rename parent props
    Also move stuff around.

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
 // Game objects and logic
 
-const get_position = function ( obj ) {
-  /** Get the relative position and dimensions of an
-  *   object's `.node` as floats.
-  * 
-  * There's something that feels better having it out here
-  *   as opposed to as a method of an instance.
-  * 
-  * @param {object} obj
-  * @param {HTML Node} obj.node
-  * 
-  * @returns {object} pos The relative position and dimensions of the object's node.
-  * @returns {float} pos.left The left position relative to the parent node. Left edge = 0.
-  * @returns {float} pos.right The right position relative to the parent node. Left edge = 0.
-  * @returns {float} pos.top The top position relative to the parent node. Left edge = 0.
-  * @returns {float} pos.bottom The bottom position relative to the parent node. Left edge = 0.
-  * @returns {float} pos.width The width of the node.
-  * @returns {float} pos.height The height of the node.
-  * @returns {float} pos.half_width The width of half the node.
-  */
-
-  return {
-    left: obj.node.offsetLeft,
-    right: obj.node.offsetLeft + obj.node.offsetWidth,
-    top: obj.node.offsetTop,
-    bottom: obj.node.offsetTop + obj.node.offsetHeight,
-    // Q: These are currently unchanging, so do they really need to be in here?
-    half_width: obj.node.offsetWidth/2,
-    width: obj.node.offsetWidth,
-    height: obj.node.offsetHeight,
-  }
-};  // Ends get_position()
-
-const have_collided = function ( obj1, obj2 ) {
-  /** Returns true if two objects are colliding, false if no. */
-  let pos1 = get_position( obj1 );
-  let pos2 = get_position( obj2 );
-
-  // TODO: Check for collision...
-};  // Ends have_collided()
-
-
 // =====================
 // =====================
 // Classes
@@ -56,14 +15,17 @@ class Game {
   * Not sure this needs to be a class right now, but
   *   might be useful in future.
   */
-  constructor () {
+  constructor ( outcome_node ) {
     this.paused = false;
-    this.outcome_node = document.querySelector(`.outcome`);
+    this.outcome_node = outcome_node;
     // Storing this list in the Descenders instance and here seems wrong
     this.descenders = null;
     this.player = null;
     // Should this be a prop of the Player instance?
     this.player_status = `playing`;
+
+    // Pause when the pause key is pressed
+    document.body.addEventListener( `keydown`, this.pause.bind( this ));
   }  // Ends game.constructor()
 
   pause ( event ) {
@@ -85,17 +47,18 @@ class Game {
 
 class Container {
   constructor ( selector ) {
-    /** An HTML node constraining the game entities that it contains.
+    /** An object containing an HTML node constraining the game entities that it contains.
+    *   They must stay inside it.
     * 
     * @params {str} selector HTML selector of the pre-existing DOM node
     */
     this.node = document.body.querySelector( selector );
     // These will always be the same
     this.left = 0;
-    this.right = this.node.offsetWidth;
+    this.width = this.node.offsetWidth;
     this.top = 0;
-    this.bottom = this.node.offsetHeight;
-    this.half_width = this.right/2;
+    this.height = this.node.offsetHeight;
+    this.half_width = this.width/2;
   }
 };  // Ends Container{}
 
@@ -114,26 +77,28 @@ class Mover {
     this.parent = parent;
   }  // Ends mover.constructor()
 
-  move_x () {
+  move_x ( vector ) {
     /** Move in the given direction. -1 moves left, 1 moves right. */
+    // TODO: Change vector to an argument instead
     if ( !this.game.paused ) {
       let this_pos = get_position( this );
-      this.node.style.left = `${ this_pos.left + (this.x_distance * this.x_vector) }px`;
+      this.node.style.left = `${ this_pos.left + (this.x_distance * vector ) }px`;
     }
   }
 
-  move_y () {
+  move_y ( vector ) {
     /** Move in the given direction. -1 moves up, 1 moves down. */
+    // TODO: Change vector to an argument instead
     if ( !this.game.paused ) {
       let this_pos = get_position( this );
-      this.node.style.top = `${ this_pos.top + (this.y_distance * this.y_vector) }px`;
+      this.node.style.top = `${ this_pos.top + (this.y_distance * vector ) }px`;
     }
   }
 
   hits_parent_left () {
-    /** Return whether or not the mover's node hits the left side of the parent.
+    /** Return whether or not the mover's node reaches the left edge of the parent.
     * 
-    * @returns {bool} Whether the left side of the mover hit the left side of the parent.
+    * @returns {bool} Whether the left edge of the mover reaches the left edge of the parent.
     */
     let obj_pos = get_position( this );
 
@@ -146,14 +111,14 @@ class Mover {
   }  // Ends mover.hits_parent_left()
 
   hits_parent_right () {
-    /** Return whether or not the mover's node hits the right side of the parent.
+    /** Return whether or not the mover's node reaches the right edge of the parent.
     * 
-    * @returns {bool} Whether the right side of the mover hit the right side of the parent.
+    * @returns {bool} Whether the right edge of the mover hit the right edge of the parent.
     */
     let obj_pos = get_position( this );
 
     // Uses the position of the mover relative to its parent.
-    if ( obj_pos.right >= this.parent.right ) {
+    if ( obj_pos.right >= this.parent.width ) {
       return true;
     } else {
       return false;
@@ -164,7 +129,7 @@ class Mover {
 
 
 class Player extends Mover {
-  /** A player-controlled avatar that can move left and right and fire a "bullet". */
+  /** A player-controlled avatar that can move left and right and fire "bullets". */
   constructor ( game, parent ) {
     /**
     * @param {HTML Node} parent Instance of parent that contains the player
@@ -176,7 +141,7 @@ class Player extends Mover {
 
     this.place_center();
 
-    // Whether the parent can move left and right
+    // Whether the player can move left and right
     this.left_disabled = false;
     this.right_disabled = false;
     // Distance divides exactly in parent so the avatar stops right
@@ -189,9 +154,9 @@ class Player extends Mover {
     let player = this;
     // `keydown` fires repeatedly, but the repeat is a bit slow for what I'd like
     document.addEventListener( `keydown`, function ( event, target ) {
-      if ( event.keyCode === 37 ) {
+      if ( event.keyCode === 37 ) {  // left arrow
         player.move_left();
-      } else if ( event.keyCode === 39 ) {
+      } else if ( event.keyCode === 39 ) {  // right arrow
         player.move_right();
       }
     });
@@ -209,8 +174,7 @@ class Player extends Mover {
 
     // If the player hasn't hit the left edge of the parent
     if ( !this.left_disabled ) {
-      this.x_vector = -1;
-      this.move_x();
+      this.move_x( -1 );
 
       // If the player has _now_ hit the edge of the parent
       if ( this.hits_parent_left( this, this.parent ) ) {
@@ -230,8 +194,7 @@ class Player extends Mover {
 
     // If the player hasn't hit the right edge of the parent
     if ( !this.right_disabled ) {
-      this.x_vector = 1;
-      this.move_x();
+      this.move_x( 1 );
 
       // If the player has _now_ hit the edge of the parent
       if ( this.hits_parent_right() ) {
@@ -241,7 +204,8 @@ class Player extends Mover {
     }  // ends if not right_disabled
 
     // Make sure player can now move left again
-    // TODO: Watch out this doesn't get enabled when pausing game
+    // TODO: Watch out this doesn't get enabled when pausing game, allowing
+    // the player to move right after pausing.
     this.left_disabled = false;
 
   }  // Ends player.move_right()
@@ -264,7 +228,7 @@ class Descenders {
     */
     // Just use global `game`?
     this.game = game;
-    this.x_vector = -1
+    this.x_vector = -1  // left first
 
     // Make all children
     this.rows = [
@@ -292,6 +256,9 @@ class Descenders {
     // or do the Math.
     let all_stop = this.game.player_status === `won`
       || this.game.player_status === `lost`;
+
+    // if (all_stop) {return;}
+
     if ( !all_stop ) {
 
       let did_hit_wall = false;
@@ -299,11 +266,11 @@ class Descenders {
 
       for ( let row of this.rows ) {
         for ( let descender of row.descenders ) {
+          // Number of descenders will affect speed of travel
           descender_count += 1;
 
           // Hmm, this is getting a bit messy just for potential future fun...
-          descender.x_vector = this.x_vector;
-          descender.move_x();
+          descender.move_x( this.x_vector );
 
           // Prepare to change direction if needed
           if ( descender.hits_parent_left() ) {
@@ -315,8 +282,7 @@ class Descenders {
         }  // end for each descender in row
       }  // end for each row
 
-      // After all the descenders have moved,
-      // if at least one of them hit a wall
+      // After all the descenders have moved, if at least one of them hit a wall...
       if ( did_hit_wall ) {
         // All should move in the opposite direction in next loop
         this.x_vector *= -1;
@@ -324,12 +290,12 @@ class Descenders {
         for ( let row of this.rows ) {
           for ( let descender of row.descenders ) {
             // Move down
-            descender.y_vector = 1;
-            descender.move_y();
+            descender.move_y( 1 );
 
+            // TODO: Need to wait to do this until after all have moved down
             // Check if they any hit the ground yet
             if ( descender.hits_parent_floor() ) {
-              if ( descender_count === 50 ) {
+              if ( descender_count === 50 ) {  // hard coded magic number for now
                 this.game.player_status = `won`;
               } else {
                 this.game.player_status = `lost`;
@@ -351,8 +317,9 @@ class Descenders {
 
       // Enemies move faster the fewer there are
       // Maximum speed (6ms) still allows the user to catch up if they
-      // have a long enough time to do it.
-      this.wait = ((descender_count - 1) * 2) + 6;
+      // have a long enough time to do it. 2ms more for each descender after the first (When
+      // there is only 1 descender, it'll be fastest. Haven't handled 0 descenders yet.)
+      this.wait = ((descender_count - 1) * 2) + 6;  // hard coded magic number
       // Loop again
       setTimeout( this.move_all.bind(this), this.wait );
 
@@ -372,15 +339,12 @@ class DescenderRow {
     /**
     * @param {obj} parent The object containing this row.
     * @param {HTML Node} parent.node HTML node of the parent object.
-    * @param {number} parent.right
+    * @param {number} parent.width
     * @param {number} parent.left
     * @param {str} type At the moment, the size of the row's descenders.
     * @param {int} row_num The vertical position of the row relative to other rows.
     *   Top is 1.
     */
-    // Start moving horizontally in this direction
-    this.x_vector = -1;
-
     // Keep everything in multiples of 2? With Math.
     let num_cols = 10;  // Could pass this as an argument instead.
     let column_width = 50;
@@ -388,8 +352,8 @@ class DescenderRow {
 
     // Put each descender in the middle of its column. Include side_padding
     // to give room for horizontal movement. More dynamic in future?
-    let side_padding = (parent.right - row_width)/2;
-    let start = parent.right - row_width - side_padding;
+    let side_padding = (parent.width - row_width)/2;
+    let start = parent.width - row_width - side_padding;
     let curr_col_middle = start + (column_width/2);
 
     this.descenders = [];
@@ -415,9 +379,9 @@ class Descender extends Mover {
     /**
     * @param {obj} parent The object containing this row.
     * @param {HTML Node} parent.node HTML node of the parent object.
-    * @param {number} parent.right X coord of the right edge of the descender container.
-    * @param {number} parent.left X coord of the left edge of the descender container.
-    * @param {number} parent.bottom Y coord of the bottom edge of the descender container.
+    * @param {number} parent.left Always 0 to this child.
+    * @param {number} parent.width X coord of the right edge relative to this child.
+    * @param {number} parent.height Y coord of the bottom edge of the container (top is 0).
     * @param {str} type At the moment, the size of the row's descenders.
     *   'small', 'medium', or 'big'
     */
@@ -430,7 +394,6 @@ class Descender extends Mover {
     // Horizontal distance whenever it moves
     this.x_distance = 2;
     // Vertical direction and distance whenever it moves
-    this.y_vector = 1;
     this.y_distance = 10;
 
     // // For testing - speed up to get to end
@@ -439,15 +402,15 @@ class Descender extends Mover {
   }  // end descender.constructor()
 
   hits_parent_floor () {
-    /** Return whether or not the mover's node hit the bottom of the parent.
+    /** Return whether or not the mover's node has reached the bottom edge of the parent.
     * 
-    * @returns {bool} Whether the bottom of the mover hit the bottom of the parent.
+    * @returns {bool} Whether the bottom of the mover has reached the bottom of the parent.
     */
 
     let obj_pos = get_position( this );
 
     // Uses the position of the mover relative to its parent.
-    if ( obj_pos.bottom >= this.parent.bottom ) {
+    if ( obj_pos.bottom >= this.parent.height ) {
       return true;
     } else {
       return false;
@@ -457,6 +420,55 @@ class Descender extends Mover {
 }  // Ends Descender{}
 
 
+
+// ===========================================
+// General Helpers
+// ===========================================
+
+const get_position = function ( obj ) {
+  /** Get the relative position and dimensions of an
+  *   object's `.node` as floats.
+  * 
+  * TODO: "Position" isn't a great word for stuff in here, but I don't know
+  *   a better one? "box"? "rectangle"?
+  * 
+  * There's something that feels better having it out here
+  *   as opposed to as a method of an instance.
+  * 
+  * @param {object} obj
+  * @param {HTML Node} obj.node
+  * 
+  * @returns {object} pos The relative position and dimensions of the object's node.
+  * @returns {float} pos.left The position of the object's left edge relative to the parent node. Left edge = 0.
+  * @returns {float} pos.right The position of the object's right edge relative to the parent node. Left edge = 0.
+  * @returns {float} pos.top The position of the object's top edge relative to the parent node. Top edge = 0.
+  * @returns {float} pos.bottom The position of the object's bottom edge relative to the parent node. Top edge = 0.
+  * @returns {float} pos.width The width of the node.
+  * @returns {float} pos.height The height of the node.
+  * @returns {float} pos.half_width The width of half the node.
+  */
+
+  return {
+    left: obj.node.offsetLeft,
+    right: obj.node.offsetLeft + obj.node.offsetWidth,
+    top: obj.node.offsetTop,
+    bottom: obj.node.offsetTop + obj.node.offsetHeight,
+    // Q: These are currently unchanging, so do they really need to be in here?
+    width: obj.node.offsetWidth,
+    height: obj.node.offsetHeight,
+    half_width: obj.node.offsetWidth/2,
+  }
+};  // Ends get_position()
+
+const have_collided = function ( obj1, obj2 ) {
+  /** Returns true if two objects are colliding, false if no. */
+  let pos1 = get_position( obj1 );
+  let pos2 = get_position( obj2 );
+
+  // TODO: Check for collision...
+};  // Ends have_collided()
+
+
 // ===========================================
 // ===========================================
 // ===========================================
@@ -464,7 +476,7 @@ class Descender extends Mover {
 // Place the pieces and start the game
 
 // Game state
-let game = new Game();
+let game = new Game( document.querySelector( `.outcome` ) );
 // Do this in Player?
 let screen = new Container( `.screen` );
 let player = new Player( game, screen );
@@ -472,6 +484,3 @@ let player = new Player( game, screen );
 let descent_space = new Container( `.descent_space` );
 let descenders = new Descenders( game, descent_space );
 // game.paused = false;
-
-document.body.addEventListener( `keydown`, game.pause.bind( game ));
-
